b315e7e98b303105883edd3ff4b12f16ac568d51
Willian <willianwroblewski@gmail.com>
2019-06-04 17:27:14 -0300

    Contacts change

--- src/root-application/root-application.js
+++ src/root-application/root-application.js
@@ -2,7 +2,7 @@ import { start, registerApplication } from 'single-spa'
 
 const hashPrefix = prefix => location => location.hash.startsWith(`#${prefix}`)
 
-registerApplication('main', () => import('../contacts/index.js'), hashPrefix('/contacts'))
+registerApplication('main', () => import('../contacts/index.js'), hashPrefix('/'))
 registerApplication('nav', () => import('../nav/index.js'), hashPrefix('/'))
 
 start()
